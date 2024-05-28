02d52d7ac121f06d088de7197a6650a65064a22b
Cristian Amancio <cristian_amancio@bhdleon.com.do>
2024-05-28 19:22:24 -0400

    Agregando directorio resources

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,9 @@ R
 "src/main/java/pages/ScorePage.java,8\c\8c380d4e82232d8a8450c1f4c0fb915f5d46b4e9
 S
 #src/main/java/pages/PlayerPage.java,5\e\5e865b07481881edb4064ebe1afea4b41594e5f8
+N
+test-output/testng-results.xml,0\0\00e0b2ae7f44da7a9e12fd1418e328ac78839661
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+I
+src/test/java/Picheo.java,e\a\ea26c03fe4e0bce172d3231d8e69052520fbdd4d
--- src/test/java/Picheo.java
+++ src/test/java/Picheo.java
@@ -15,22 +15,18 @@ public class Picheo extends BaseTest {
         StringBuilder content = new StringBuilder();
 
         for (int i = 1; i <= scorePage.getCountMatches(); i++) {
-            if(i!=9 && i!=15) {
-                content.append("\nteam: ").append(scorePage.getNameTeam(i)).append("\n").append("pitcher: ").append(scorePage.getNamePitcher(i)).append("\n").append("\n");
-                scorePage.selectPitcher(i);
-                playerPage.clickButonGameLogs();
-                playerPage.getGameLogs();
-                content.append("Partidos lanzados: ").append(playerPage.getTotalGamePitched()).append("\n");
-                content.append("Promedio Ining: ").append(playerPage.getPorcentIP()).append("\n");
-                content.append("Promedio carrera : ").append(playerPage.getPorcentR()).append("\n");
-                content.append("Promedio ponche: ").append(playerPage.getPorcentSO()).append("\n");
-                scorePage.goToPageMlb();
-            }
-            
+            content.append("\nteam: ").append(scorePage.getNameTeam(i)).append("\n").append("pitcher: ").append(scorePage.getNamePitcher(i)).append("\n").append("\n");
+            scorePage.selectPitcher(i);
+            playerPage.clickButonGameLogs();
+            playerPage.getGameLogs();
+            content.append("Partidos lanzados: ").append(playerPage.getTotalGamePitched()).append("\n");
+            content.append("Promedio Ining: ").append(playerPage.getPorcentIP()).append("\n");
+            content.append("Promedio carrera : ").append(playerPage.getPorcentR()).append("\n");
+            content.append("Promedio ponche: ").append(playerPage.getPorcentSO()).append("\n");
+            scorePage.goToPageMlb();
         }
 
         HelperFile.getInstance().createFile(System.getProperty("user.dir") + "\\src\\main\\resources\\picheo\\PicheoToday", content.toString(), "txt");
-
 
     }
 
